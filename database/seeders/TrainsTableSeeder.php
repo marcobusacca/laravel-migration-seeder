d5962b102eb198c3d82cfe8753b830e9f156f708
marcobusacca <marcobusacca01@gmail.com>
2023-07-27 13:07:14 +0200

    [FEAT] linked Faker in TrainsTableSeeder

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
+use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
