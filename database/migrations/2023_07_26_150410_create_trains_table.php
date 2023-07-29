52df124f98db06a5589542a374cf2db4690fbae2
marcobusacca <marcobusacca01@gmail.com>
2023-07-29 12:07:43 +0200

    [BONUS] database passengers table migration

--- database/migrations/2023_07_26_150410_create_trains_table.php
+++ database/migrations/2023_07_26_150410_create_trains_table.php
@@ -15,16 +15,27 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            // CODICE TRENO
             $table->string('train_code', 10);
+            // AZIENDA
             $table->string('company', 50);
+            // STAZIONE DI PARTENZA
             $table->string('departure_station', 50);
+            // STAZIONE DI ARRIVO
             $table->string('arrival_station', 50);
+            // DATA DI PARTENZA
             $table->date('departure_date');
+            // ORARIO DI PARTENZA
             $table->time('departure_time');
+            // DATA DI ARRIVO
             $table->date('arrival_date');
+            // ORARIO DI ARRIVO
             $table->time('arrival_time');
+            // NUMERO CARROZZE
             $table->tinyInteger('coaches_number')->nullable();
+            // IN ORARIO (SI, NO)
             $table->boolean('on_time')->default(true);
+            // CANCELLATO (SI, NO)
             $table->boolean('deleted')->default(false);
             $table->timestamps();
         });
---
+++ database/migrations/2023_07_29_100129_create_passengers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('passengers', function (Blueprint $table) {
+            $table->id();
+            // CODICE FISCALE
+            $table->string('tax_code', 16);
+            // NOME
+            $table->string('name', 20);
+            // COGNOME
+            $table->string('surname', 20);
+            // DATA DI NASCITA
+            $table->date('date_of_birth');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('passengers');
+    }
+};
