2807ef798062d3c77fe33ed43ac36ebbe63e0c9b
marcobusacca <marcobusacca01@gmail.com>
2023-07-29 12:11:17 +0200

    [BONUS] database passengers table seeder

---
+++ database/seeders/PassengersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+// IMPORTARE IL MODEL
+use Faker\Generator as Faker;
+
+class PassengersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i = 1; $i <= 4; $i++){
+        }
+    }
+}
